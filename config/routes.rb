802a7484a775ddf00fa9b9914cadd8baa766968e
hatanatsu1993 <086.yga4jjguaf5@gmail.com>
2025-05-02 17:49:48 +0900

    指摘事項の反映（３）

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
 
-  resources :items, only:[:index, :new, :show]
+  resources :items, only:[:index, :new, :create, :show]
 end
